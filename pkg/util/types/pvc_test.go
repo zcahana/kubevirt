0149b67f3bdf16772dbca372799a31e1ab8a57a1
Zvi Cahana <zvic@il.ibm.com>
2021-09-22 12:13:38 +0300

    Unit tests for provisioning failure detection
    Signed-off-by: Zvi Cahana <zvic@il.ibm.com>

--- pkg/util/types/pvc_test.go
+++ pkg/util/types/pvc_test.go
@@ -22,7 +22,6 @@ package types
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
 	kubev1 "k8s.io/api/core/v1"
 	storagev1 "k8s.io/api/storage/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
